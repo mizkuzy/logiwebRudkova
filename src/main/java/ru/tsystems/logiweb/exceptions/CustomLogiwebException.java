6046ed477c48219f3120f2116da60a8bf0a55782
mizkuzy <mizkuzy@yandex.ru>
2016-01-08 20:49:49 +0300

    CustomException

---
+++ src/main/java/ru/tsystems/logiweb/exceptions/CustomLogiwebException.java
@@ -0,0 +1,12 @@
+package ru.tsystems.logiweb.exceptions;
+
+/**
+ * A custom exception to be thrown.
+ */
+public class CustomLogiwebException extends RuntimeException {
+
+    public CustomLogiwebException(String message) {
+        super(message);
+    }
+
+}
