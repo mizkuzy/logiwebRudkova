8221551da5a38933de310d08ec47a5b43cd48641
mizkuzy <mizkuzy@yandex.ru>
2016-01-11 23:12:19 +0300

    DriverDao

--- src/main/java/ru/tsystems/logiweb/dao/API/DriverGenericDAO.java
+++ src/main/java/ru/tsystems/logiweb/dao/API/DriverGenericDAO.java
@@ -1,9 +1,6 @@
 package ru.tsystems.logiweb.dao.API;
 
 import ru.tsystems.logiweb.entities.Driver;
-import ru.tsystems.logiweb.entities.Order;
-
-import java.util.List;
 
 public interface DriverGenericDAO extends GenericDAO<Driver, Integer>{
 
