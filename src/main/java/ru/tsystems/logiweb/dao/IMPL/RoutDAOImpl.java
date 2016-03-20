fd04d9edec39f57aa53f27a14e928280bfb4a2f3
mizkuzy <mizkuzy@yandex.ru>
2016-03-20 20:07:39 +0300

    improving method getCorrespondingCities()

--- src/main/java/ru/tsystems/logiweb/dao/IMPL/RoutDAOImpl.java
+++ src/main/java/ru/tsystems/logiweb/dao/IMPL/RoutDAOImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Repository;
 import ru.tsystems.logiweb.exceptions.CustomLogiwebException;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * An implementation of RoutDAO API.
@@ -18,6 +20,7 @@ public class RoutDAOImpl extends GenericDAOImpl<Rout, Integer> implements RoutGe
 
     @PersistenceContext(type = PersistenceContextType.EXTENDED)
     private EntityManager entityManager;
+
 
     /**
      * Gets required rout.
@@ -39,8 +42,30 @@ public class RoutDAOImpl extends GenericDAOImpl<Rout, Integer> implements RoutGe
 
         } catch (PersistenceException ex) {
 
-            logger.info(ex.getMessage() + "Wrong rout: city1 - " + city1 + "; city2 - " + city2, ex);
-            throw new CustomLogiwebException("Rout with parameters: city1 - " + city1 + "; city2 - " + city2+" not found.",ex.getMessage());
+            logger.warn(ex.getMessage() + "Wrong rout: city1 - " + city1 + "; city2 - " + city2, ex);
+            throw new CustomLogiwebException("Rout with parameters: city1 - " + city1 + "; city2 - " + city2 +
+                    " not found.", ex.getMessage());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<String> getCorrespondingCitiesForCityFrom(String cityFrom) throws CustomLogiwebException {
+        try {
+            Query query = entityManager.createQuery("select rout from  Rout rout where rout.city1=:cityFrom");
+            query.setParameter("cityFrom",cityFrom);
+
+            List<Rout> routs = query.getResultList();
+            List<String> citiesTo = new ArrayList<>(routs.size());
+            for (Rout rout : routs) {
+                citiesTo.add(rout.getCity2());
+            }
+            return citiesTo;
+        } catch (PersistenceException ex) {
+            logger.warn(ex.getMessage() + "cityFrom=" + cityFrom, ex);
+            throw new CustomLogiwebException("cityFrom=" + cityFrom, ex.getMessage());
         }
     }
 }
