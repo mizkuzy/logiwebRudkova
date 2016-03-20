9cda563a07bbda4944ec1cdd56261286f8f0b656
mizkuzy <mizkuzy@yandex.ru>
2016-03-20 20:07:03 +0300

    improving method getCorrespondingCities()

--- src/main/java/ru/tsystems/logiweb/service/API/RoutService.java
+++ src/main/java/ru/tsystems/logiweb/service/API/RoutService.java
@@ -3,6 +3,7 @@ package ru.tsystems.logiweb.service.API;
 import ru.tsystems.logiweb.entities.Rout;
 
 import javax.persistence.PersistenceException;
+import java.util.HashSet;
 import java.util.List;
 
 public interface RoutService extends GenericService<Rout, Integer> {
@@ -11,25 +12,10 @@ public interface RoutService extends GenericService<Rout, Integer> {
 
     List<String> getCities();
 
-    List<String> getCitiesForSPb();
-
-    List<String> getCitiesForVelikyNovgorod();
-
-    List<String> getCitiesForPskov();
-
-    List<String> getCitiesForPetrozavodsk();
-
-    List<String> getCitiesForArhangelsk();
-
-    List<String> getCitiesForVologda();
-
-    List<String> getCitiesForSiktivkar();
-
-    List<String> getCitiesForNaryanMar();
-
-    List<String> getCitiesForMurmansk();
-
-    List<String> getCitiesForKaliningrad();
-
-    List<String> getCitiesForCherepovec();
+    /**
+     * Returns set of cities where we can deliver goods from cityFrom.
+     * @param cityFrom
+     * @return set of cities
+     */
+    List<String> getCorrespondingCitiesForCity1(String cityFrom);
 }
