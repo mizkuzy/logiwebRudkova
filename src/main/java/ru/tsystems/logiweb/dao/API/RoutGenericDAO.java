32cd2a44c5709666d62d3efdd58c83aef007829a
mizkuzy <mizkuzy@yandex.ru>
2016-03-20 20:07:21 +0300

    improving method getCorrespondingCities()

--- src/main/java/ru/tsystems/logiweb/dao/API/RoutGenericDAO.java
+++ src/main/java/ru/tsystems/logiweb/dao/API/RoutGenericDAO.java
@@ -3,7 +3,18 @@ package ru.tsystems.logiweb.dao.API;
 import ru.tsystems.logiweb.entities.Rout;
 import ru.tsystems.logiweb.exceptions.CustomLogiwebException;
 
+import java.util.List;
+
 public interface RoutGenericDAO extends GenericDAO<Rout, Integer> {
 
     Rout getByCities(String city1, String city2) throws CustomLogiwebException;
+
+    /**
+     * Makes query to DB to get set of cities corresponding cityFrom.
+     *
+     * @param cityFrom
+     * @return set of cities
+     */
+    List<String> getCorrespondingCitiesForCityFrom(String cityFrom);
+
 }
